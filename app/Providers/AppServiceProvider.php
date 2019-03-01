67da973a44fc46083ff9359f01f224d959cf874b
Wiwat Srisattha <wiwat_srt@hotmail.com>
2019-03-01 21:28:24 +0700

    Remove debugbar

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -30,8 +30,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        if ($this->app->isLocal()) {
-            $this->app->register(\Barryvdh\Debugbar\ServiceProvider::class);
-        }
+        //
     }
 }
