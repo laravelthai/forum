17bde049523d871620ffc1b85164c172e6d63704
Wiwat Srisattha <wiwat_srt@hotmail.com>
2019-03-02 22:22:15 +0700

    Pretties code

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
-const mix = require('laravel-mix');
+const mix = require('laravel-mix')
 
 /*
  |--------------------------------------------------------------------------
@@ -13,8 +13,8 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .extract(['axios', 'bootstrap', 'jquery', 'popper.js', 'moment', 'vue'])
-    .sass('resources/sass/app.scss', 'public/css');
+    .sass('resources/sass/app.scss', 'public/css')
 
 if (mix.inProduction()) {
-    mix.version();
+    mix.version()
 }
